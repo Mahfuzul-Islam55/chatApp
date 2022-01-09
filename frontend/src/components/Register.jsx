03b81fb4ae7b58ec34a851b48921419be72c4322
shimulsust999 <shimulsust999@gmail.com>
2022-01-09 18:22:06 +0600

    adding authaction for registration

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,8 +1,8 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
-
 export default function Register() {
 
+    
     const [state,setState]=useState({
         userName:'',
         email:'',
---
+++ frontend/src/store/actions/authAction.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const userRegister=(data)=>{
+    return async(dispatch)=>{
+
+        const config={
+            headers:{
+                'Content-Type':'application/json'
+            }
+        }
+        try{
+            const response=await axios.post('/api/messenger/user-register',data,config);
+        }catch(error){
+            
+        }
+    }
+}
