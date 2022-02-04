7ce2d459cb81aa079be18aeb151e2c24637517a9
shimulsust999 <shimulsust999@gmail.com>
2022-02-04 17:30:31 +0600

    showing seen unseen icon and name,last message,time in the friendlist

--- frontend/src/components/Friends.jsx
+++ frontend/src/components/Friends.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
+import moment from 'moment';
 export const Friends = ({friend}) => {
+    const {myInfo}=useSelector(state=>state.auth);
   return (
         <div className='friend'>
             <div className="friend-image">
@@ -8,8 +10,32 @@ export const Friends = ({friend}) => {
                     <img src={`/image/${friend.friendInfo.image}`} alt=""/>
                 </div>
             </div>
-            <div className="friend-name">
-                <h4>{friend.friendInfo.userName}</h4>
+            <div className="friend-name-seen">
+                <div className="friend-name">
+                    <h4>{friend.friendInfo.userName}</h4>
+                    <div className="message-time">
+                        {
+                          friend.messageInfo && friend.messageInfo.senderId===myInfo.id?<span>You: </span>:<span>{friend.friendInfo.userName}: </span>
+                        }
+                        {
+                            friend.messageInfo && friend.messageInfo.message.text?<span>{friend.messageInfo.message.text.slice(0,10)}</span>:  friend.messageInfo && friend.messageInfo.message.image?<span>send an image</span>:<span>connect to you</span>
+                        }
+                        {
+                            friend.messageInfo?<span> {moment(friend.messageInfo.createdAt).startOf('mini').fromNow()}</span>:<span> {moment(friend.createdAt).startOf('mini').fromNow()}</span>
+                        }
+                    </div>
+                </div>
+                    {
+                        friend.messageInfo && myInfo.id===friend.messageInfo.senderId?
+                            <div className="seen-unseen-icon">
+                                <img src={`/image/${friend.friendInfo.image}`} alt=""/>
+                            </div>:
+                            <div className="seen-unseen-icon">
+                                <div className="seen-icon">
+
+                                </div>
+                            </div>
+                        }
             </div>
         </div>
         );
