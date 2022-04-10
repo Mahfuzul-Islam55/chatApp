c13636a5709eb35d2df6683183d9397a1047c310
shimulsust999 <shimulsust999@gmail.com>
2022-04-10 15:01:21 +0600

    redirect to login page if user is not logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import {BrowserRouter as Router,Route,Switch} from'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import { Messenger } from './components/Messenger';
+import ProtectRoute from './components/ProtectRoute';
 function App() {
   return (
    <Router>
      <Switch>
        <Route exact path="/messenger/register" component={Register}></Route>
        <Route exact path="/messenger/login" component={Login}></Route>
+       <ProtectRoute exact path="/" component={Messenger}></ProtectRoute>
        <Route exact path="/" component={Messenger}></Route>
      </Switch>
 
